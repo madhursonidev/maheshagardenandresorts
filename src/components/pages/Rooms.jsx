dcbeb282dd4281b509628e1941f702926ee12c09
madhursonidev <madhursonidev@gmail.com>
2022-12-09 02:59:08 +0530

    Working on Rooms

--- src/components/pages/Rooms.jsx
+++ src/components/pages/Rooms.jsx
@@ -1,41 +1,50 @@
 import React from "react";
+import Button from "./core_component/Button";
 import InputText from "./core_component/InputText";
-import { MdBed } from "react-icons/md";
+import { MdBed, MdPhone } from "react-icons/md";
 import { RiCalendar2Fill } from "react-icons/ri";
+import { GoPerson } from "react-icons/go";
 
 const Rooms = () => {
   return (
-    <div className="flex flex-row justify-center">
-      <h1 className="text-5xl font-serif font-medium">BOOK YOUR RESORT ROOM</h1>
-      <p>There are three types of room available.</p>
-      <form action="" method="post">
-        <div className="flex">
-          <div className="flex flex-col">
-            <label className="font-medium" for="room">
-              <MdBed /> ROOM
-            </label>
-            <InputText name="room" placeholder="Choose Room Type" />
+    <div className="flex bg-gold-500 px-5 py-28 justify-center text-center">
+      <div>
+        <h1 className="text-5xl font-serif font-medium">
+          BOOK YOUR RESORT ROOM
+        </h1>
+        <p>There are three types of room available.</p>
+        <form action="" method="post" className="mt-12">
+          <div className="flex text-left">
+            <div className="flex flex-col">
+              <label className="font-medium flex" for="room">
+                <MdBed /> ROOM
+              </label>
+              <InputText name="room" placeholder="Choose Room Type" />
+            </div>
+            <div className="flex flex-col">
+              <label className="font-medium flex" for="date">
+                <RiCalendar2Fill /> DATE
+              </label>
+              <InputText name="date" placeholder="dd/mm/yyyy" />
+            </div>
+            <div className="flex flex-col">
+              <label className="font-medium flex" for="name">
+                <GoPerson /> NAME
+              </label>
+              <InputText name="name" placeholder="Your full name" />
+            </div>
+            <div className="flex flex-col">
+              <label className="font-medium flex" for="phoneNumber">
+                <MdPhone /> PHONE NUMBER
+              </label>
+              <InputText name="phoneNumber" placeholder="Mobile number" />
+            </div>
+            <Button className="text-2xl text-black font-bold !p-4">
+              <p className="flex items-center">BOOK NOW</p>
+            </Button>
           </div>
-          <div className="flex flex-col">
-            <label className="font-medium" for="date">
-              <RiCalendar2Fill /> DATE
-            </label>
-            <InputText name="date" placeholder="dd/mm/yyyy" />
-          </div>
-          <div className="flex flex-col">
-            <label className="font-medium" for="name">
-              NAME
-            </label>
-            <InputText name="name" placeholder="Your full name" />
-          </div>
-          <div className="flex flex-col">
-            <label className="font-medium" for="phoneNumber">
-              PHONE NUMBER
-            </label>
-            <InputText name="phoneNumber" placeholder="Mobile number" />
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
