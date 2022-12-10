aa64e922fccd126a670b8923284df7ff1ebd496e
Codination <madhursonidev@gmail.com>
2022-12-11 04:25:37 +0530

    Working on Footer

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,17 +1,40 @@
 import React from "react";
+import Button from "./core_component/Button";
 
 const Footer = () => {
   return (
-    <div className="bg-blue-900">
-      <div className="container mx-auto flex justify-between">
+    <div className="bg-[#14274A] py-14">
+      <div className="container mx-auto flex justify-between items-center text-white">
         <div>
-          <p>logo</p>
-          <p>address</p>
-          <p>number</p>
+          <p>
+            <img
+              src="/assets/images/Logo_white_1.png"
+              alt="Mahesha"
+              className="py-5 "
+            />
+          </p>
+          <p className="max-w-xs">
+            Gram: Bada Bangarda, Super Coridor, Indore 453112
+          </p>
+          <p>99-260-788-12</p>
         </div>
-        <div>about us</div>
-        <div>facebook</div>
-        <div>Subscribe to our newsletter</div>
+        <div>
+          <p>About Us </p>
+          <p>Contact </p>
+          <p>Terms & Conditions</p>
+        </div>
+        <div>
+          <p> Facebook </p>
+          <p> Twitter </p>
+          <p> Instagram </p>
+        </div>
+        <div>
+          <p> Subscribe to our newsletter </p>
+          <form action="" method="post" className="flex">
+            <input type="email" placeholder="Email Address" />
+            <Button>OK</Button>
+          </form>
+        </div>
       </div>
     </div>
   );
